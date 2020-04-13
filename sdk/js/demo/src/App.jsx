eed973a33706767830fd3b1c7792e95e5176db3f
Travis James <travis@tribehealthsolutions.com>
2020-04-13 07:00:46 -0500

    compassus build

--- sdk/js/demo/src/App.jsx
+++ sdk/js/demo/src/App.jsx
@@ -229,8 +229,8 @@ class App extends React.Component {
       <Layout className="app-layout">
         <Header className="app-header">
           <div className="app-header-left">
-            <a href="https://pion.ly" target="_blank">
-              <img src="/pion-logo.svg" className="app-logo-img" />
+            <a href="https://compassus.com" target="_blank">
+              <img src="/compassus.png" className="app-logo-img" />
             </a>
           </div>
           {login ? (
@@ -362,7 +362,7 @@ class App extends React.Component {
           ) : loading ? (
             <Spin size="large" tip="Connecting..." />
           ) : (
-                <Card title="Join to Ion" className="app-login-card">
+                <Card title="Join Talk" className="app-login-card">
                   <LoginForm handleLogin={this._handleJoin} />
                 </Card>
               )}
@@ -371,8 +371,8 @@ class App extends React.Component {
         {!login && (
           <Footer className=".app-footer">
             Powered by{" "}
-            <a href="https://pion.ly" target="_blank">
-              Pion
+            <a href="https://tribehealthsolutions.com" target="_blank">
+              Tribe Health
             </a>{" "}
             WebRTC.
           </Footer>
