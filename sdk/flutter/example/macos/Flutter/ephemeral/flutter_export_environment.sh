3fd9ac1109013a45373caf0a8341e3ea2996eaf6
Travis James <travis@tribehealthsolutions.com>
2020-04-13 04:44:45 -0500

    created mobile projects and branding

---
+++ sdk/flutter/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gqadonis/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gqadonis/Projects/TribeHealth/webrtc/ion/sdk/flutter/example"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gqadonis/development/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
